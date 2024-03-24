505d6bbaee5a7f96320ffddac7d5db61e2ddbe13
Testing Akun <cybersentry2099@gmail.com>
2024-03-24 14:37:16 +0700

    make test error

--- src/hooks/useInput.js
+++ src/hooks/useInput.js
@@ -3,7 +3,7 @@ import { useState } from 'react';
 const useInput = (defaultValue) => {
   const [value, setValue] = useState(defaultValue);
   const handleValueChange = (event) => setValue(event.target.value);
-  return [value];
+  return [value, handleValueChange, error];
 };
 
 export default useInput;
